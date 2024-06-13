f460e5522268ea91a7769b5455ea15a209398039
0xABDULMALIK <abdelmalik.omar1999@gmail.com>
2024-06-13 12:46:36 +0200

    Update ResNet_Model.py

--- src/Models/ResNet_Model.py
+++ src/Models/ResNet_Model.py
@@ -9,18 +9,19 @@ def build_model():
                         include_top=False,
                         input_shape=(160,160,3)
                         )
-  
     model = Sequential([densenet,
+
                         layers.GlobalAveragePooling2D(),
                         layers.Dense(1024, activation = 'relu'),
                         layers.BatchNormalization(),
-                        layers.Dense(256, activation = 'relu'),
+                         layers.Dense(256, activation = 'relu'),
                         layers.BatchNormalization(),
+
+
                         layers.Dense(2, activation='softmax')
                         ])
-  
     model.compile(optimizer=Adam(learning_rate=0.001),
                   loss='categorical_crossentropy',
-                  metrics=['accuracy'
-                  ])
+                  metrics=['accuracy']
+                 )
     return model
