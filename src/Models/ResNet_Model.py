c5b1e31aff7a4057984891288334de2f1f7ca06d
0xABDULMALIK <abdelmalik.omar1999@gmail.com>
2024-06-13 03:26:11 +0200

    Create ResNet_Model.py

---
+++ src/Models/ResNet_Model.py
@@ -0,0 +1,26 @@
+from tensorflow.keras import layers 
+from tensorflow.keras.applications import ResNet50V2 
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.optimizers import Adam 
+
+def build_model():
+    densenet = ResNet50V2(
+                        weights='imagenet',
+                        include_top=False,
+                        input_shape=(160,160,3)
+                        )
+  
+    model = Sequential([densenet,
+                        layers.GlobalAveragePooling2D(),
+                        layers.Dense(1024, activation = 'relu'),
+                        layers.BatchNormalization(),
+                        layers.Dense(256, activation = 'relu'),
+                        layers.BatchNormalization(),
+                        layers.Dense(2, activation='softmax')
+                        ])
+  
+    model.compile(optimizer=Adam(learning_rate=0.001),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'
+                  ])
+    return model
