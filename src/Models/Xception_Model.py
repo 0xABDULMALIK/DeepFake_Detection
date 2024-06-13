3dd1dc598f648802a8e8af488ca79a408c7b59c4
0xABDULMALIK <abdelmalik.omar1999@gmail.com>
2024-06-13 14:17:09 +0200

    Create Xception_Model.py

---
+++ src/Models/Xception_Model.py
@@ -0,0 +1,27 @@
+from tensorflow.keras import layers 
+from tensorflow.keras.applications import Xception
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.optimizers import Adam 
+
+def build_model():
+    densenet = Xception(
+                        weights='imagenet',
+                        include_top=False,
+                        input_shape=(160,160,3)
+                        )
+    model = Sequential([densenet,
+
+                        layers.GlobalAveragePooling2D(),
+                        layers.Dense(1024, activation = 'relu'),
+                        layers.BatchNormalization(),
+                         layers.Dense(256, activation = 'relu'),
+                        layers.BatchNormalization(),
+
+
+                        layers.Dense(2, activation='softmax')
+                        ])
+    model.compile(optimizer=Adam(learning_rate=0.001),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy']
+                 )
+  return model
